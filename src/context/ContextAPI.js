b73bff9a059b1035bd75fba38fef4029e78649d6
kunal1729 <102245342+kunal1729@users.noreply.github.com>
2024-06-07 18:24:12 +0545

    Update ContextAPI.js

--- src/context/ContextAPI.js
+++ src/context/ContextAPI.js
@@ -101,7 +101,7 @@ export const BooksProvider = ({children}) => {
         {
             setSavedData([]);
         }
-    }, [allSavedBooks])
+    }, [allSavedBooks, getSavedData])
 
     return(
         <BooksContext.Provider value={{searchData , getSearchData, saveBook, unsaveBook,allSavedBooks, savedData , setSavedData}}>
